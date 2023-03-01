97fc3dc9f83de9630b06f10703b7b7f911294f03
augustodsgv <augustodsgv@gmail.com>
2023-03-01 16:25:12 -0300

    adicionando botoes

--- projeto/lib/main.dart
+++ projeto/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 void main() {
   runApp(MyApp());
 }
@@ -10,57 +9,79 @@ class MyApp extends StatefulWidget {
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
-
 }
 
 class _MyHomePageState extends State<MyApp> {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-      body: 
-        /*Center(
+          body:
+              /*Center(
           child: Text(
             'Bem vindo(a) ao DC!')
         )*/
-        Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Text('Bem vindo(a) ao DC!'),
-              ],
+              Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: const [
+              Text('Bem vindo(a) ao DC!'),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    print("oriente-me");
+                  },
+                  child: const Text(
+                    'Oriente-me',
+                  )),
+              ElevatedButton(
+                onPressed: () {
+                  print("cardapio RU");
+                },
+                child: const Text(
+                  'Cardápio RU',
+                ),
+              ),
+            ],
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+            ElevatedButton(
+              onPressed: () {
+                print('Saldo da carteirinha');
+              },
+              child: Text(
+                'Saldo da carteirinha',
+              ),
             ),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Text('Oriente-me'),
-                Text('Cardápio do RU')
-              ],
-            ),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Text('Saldo da carteirinha'),
-                Text('Reservar salas')
-              ],
-            ),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Text('Não me sinto bem'),
-              ],
-            ),
-          ],    
-        )
-
-      
-      ),
+            ElevatedButton(
+                onPressed: () {
+                  print('Reservar salas');
+                },
+                child: Text(
+                  'Reservar Salas',
+                ))
+          ]),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print("não me sinto bem");
+                },
+                child: Text(
+                  "Não me sinto bem",
+                ),
+              ),
+            ],
+          ),
+        ],
+      )),
     );
   }
 }
